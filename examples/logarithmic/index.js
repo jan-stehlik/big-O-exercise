0089d6422bec0ac182ddbcb57d6087172557e990
Jan Stehlik <jan.stehlik@gousto.co.uk>
2019-03-01 13:47:34 +0000

    add binary search algorithm as a logarithmic example

---
+++ examples/logarithmic/index.js
@@ -0,0 +1,22 @@
+function binarySearch(sortedArray, targetValue) {
+  let leftIndex = 0
+  let rightIndex = sortedArray.length
+  let middleIndex
+
+  while (leftIndex < rightIndex) {
+    middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+
+    if (sortedArray[middleIndex] < targetValue) {
+      leftIndex = middleIndex + 1
+    } else if (sortedArray[middleIndex] > targetValue) {
+      rightIndex = middleIndex
+    } else {
+      return middleIndex
+    }
+  }
+
+  return 'Not found'
+}
+
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+console.log(binarySearch(testArray, 8))
