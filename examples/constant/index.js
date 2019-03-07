a732f545ccf5c1384f7f036b5e8101519f0a7779
Jan Stehlik <jan.stehlik@gousto.co.uk>
2019-03-07 13:11:45 +0000

    fix getLastItemFromArray

--- examples/constant/index.js
+++ examples/constant/index.js
@@ -1,4 +1,4 @@
 // as the size of an array variable grows the time to perform this function will remain constant
 function getLastItemFromArray(array) {
-  return array[array.length]
+  return array[array.length - 1]
 }
